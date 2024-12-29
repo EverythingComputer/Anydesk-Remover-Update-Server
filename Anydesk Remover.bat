13ce83469e0421297792539ca3c595a41ca5a4fd
EverythingComputer <joshuavoelkel355@gmail.com>
2024-12-29 17:20:08 -0600

    Add files via upload

---
+++ Anydesk Remover.bat
@@ -0,0 +1,5 @@
+@echo off
+echo If you see a error "Windows couldnt find AnyDesk.exe then your good. If AnyDesk opens, then you have anydesk on your pc.
+pause
+start C:\ProgramFiles(x86)\AnyDesk\AnyDesk.exe
+end
